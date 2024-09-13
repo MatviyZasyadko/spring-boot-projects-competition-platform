cd7e326089ca910ab8d69762a63315351ec4411c
MatviyZasyadko <105881181+MatviyZasyadko@users.noreply.github.com>
2024-09-13 18:34:59 +0300

    Add files via upload

---
+++ ProjectsCompetitionPlatform/src/main/java/com/SpringBoot/ProjectsCompetitionPlatform/ProjectsCompetitionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ProjectsCompetitionPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsCompetitionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsCompetitionPlatformApplication.class, args);
+	}
+
+}
---
+++ ProjectsCompetitionPlatform/src/test/java/com/SpringBoot/ProjectsCompetitionPlatform/ProjectsCompetitionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ProjectsCompetitionPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsCompetitionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
