50b0215c479c3d9a51d32a07cf4ac0b6cd7bf9ac
Zasyadko Matviy <matvey.z.2004@icloud.com>
2024-09-15 16:37:07 +0300

    Added two base classes

---
+++ ProjectsCompetitionPlatform/src/main/java/com/SpringBoot/ProjectsCompetitionPlatform/users/Developer.java
@@ -0,0 +1,9 @@
+package com.SpringBoot.ProjectsCompetitionPlatform.users;
+
+public class Developer extends User {
+
+    public Developer(String firstName, String lastName){
+        super(firstName, lastName);
+    }
+
+}
