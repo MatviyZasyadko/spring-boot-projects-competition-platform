8053684dcef9841bfb50c09e5d784776c0fa893f
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-11-27 23:49:25 +0200

    added a modal window for applying project to competition

--- ProjectsCompetitionPlatform/build.gradle.kts
+++ ProjectsCompetitionPlatform/build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.h2database:h2")
+    //implementation("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -38,6 +38,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("com.ukma.edu:spring-boot-starter-cloudinary:0.0.1-SNAPSHOT")
--- ProjectsCompetitionPlatform/src/main/java/com/ukma/competition/platform/ProjectsCompetitionPlatformApplication.java
+++ ProjectsCompetitionPlatform/src/main/java/com/ukma/competition/platform/ProjectsCompetitionPlatformApplication.java
@@ -45,121 +45,121 @@ public class ProjectsCompetitionPlatformApplication implements CommandLineRunner
 
     @Override
     public void run(String... args) {
-        UserEntity admin = UserEntity.builder()
-            .email("admin@mail.com")
-            .fullName("Volodymyr Havryliuk")
-            .password(passwordEncoder.encode("admin"))
-            .userRole(UserRole.ADMIN)
-            .authenticationProvider(AuthenticationProvider.NATIVE)
-            .build();
-
-        ProjectEntity firstProject = ProjectEntity.builder()
-            .name("Competitors")
-            .shortDescription("Competitors is an innovative platform where users can showcase their projects, " +
-                              "participate in various competitions, and win exciting prizes. Designed for creators, developers, and innovators.")
-            .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
-                             "participate in various competitions, and win exciting prizes. " +
-                             "Designed for creators, developers, and innovators, " +
-                             "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
-            .images(
-                new ArrayList<>() {{
-                    add(
-                        ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
-                            .main(true)
-                            .name("somename.jpg").build()
-                    );
-                }}
-            )
-            .creator(admin)
-            .build();
-
-        ProjectEntity secondProject = ProjectEntity.builder()
-            .name("Volo")
-            .shortDescription("Volo is a web platform designed to help individuals and organizations raise funds for " +
-                              "charitable causes. It simplifies the donation process, allowing users to create campaigns," +
-                              " share them with others, and collect contributions securely")
-            .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
-                             "participate in various competitions, and win exciting prizes. " +
-                             "Designed for creators, developers, and innovators, " +
-                             "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
-            .images(
-                new ArrayList<>() {{
-                    add(
-                        ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
-                            .name("somename.jpg")
-                            .main(true)
-                            .build()
-                    );
-                    add(
-                        ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
-                            .name("somename.jpg")
-                            .main(false)
-                            .build()
-                    );
-
-                    add(
-                        ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
-                            .name("somename.jpg")
-                            .main(false)
-                            .build()
-                    );
-
-                    add(
-                        ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
-                            .name("somename.jpg")
-                            .main(false)
-                            .build()
-                    );
-                }}
-            )
-            .creator(admin)
-            .build();
-
-        admin.addProject(firstProject);
-        admin.addProject(secondProject);
-        for (int i = 0; i < 19; i++) {
-            admin.addProject(firstProject);
-            firstProject = ProjectEntity.builder()
-                .name("Competitors")
-                .shortDescription("Competitors is an innovative platform where users can showcase their projects, " +
-                                  "participate in various competitions, and win exciting prizes. Designed for creators, developers, and innovators.")
-                .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
-                                 "participate in various competitions, and win exciting prizes. " +
-                                 "Designed for creators, developers, and innovators, " +
-                                 "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
-                .images(
-                    new ArrayList<>() {{
-                        add(
-                            ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
-                                .main(true)
-                                .name("somename.jpg").build()
-                        );
-                        add(
-                            ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
-                                .main(false)
-                                .name("somename.jpg").build()
-                        );
-                        add(
-                            ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
-                                .main(false)
-                                .name("somename.jpg").build()
-                        );
-                    }}
-                )
-                .creator(admin)
-                .build();
-        }
-        if (userRepository != null) {
-            userRepository.save(admin);
-        }
-
-        CompetitionEntity competitionEntity = CompetitionEntity.builder()
-            .name("first competition")
-            .description("first competition description")
-            .votingEndDate(Instant.MAX)
-            .organizer(admin)
-            .build();
-
-        competitionRepository.saveAndFlush(competitionEntity);
+      //  UserEntity admin = UserEntity.builder()
+      //      .email("admin@mail.com")
+      //      .fullName("Volodymyr Havryliuk")
+      //      .password(passwordEncoder.encode("admin"))
+      //      .userRole(UserRole.ADMIN)
+      //      .authenticationProvider(AuthenticationProvider.NATIVE)
+      //      .build();
+//
+      //  ProjectEntity firstProject = ProjectEntity.builder()
+      //      .name("Competitors")
+      //      .shortDescription("Competitors is an innovative platform where users can showcase their projects, " +
+      //                        "participate in various competitions, and win exciting prizes. Designed for creators, developers, and innovators.")
+      //      .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
+      //                       "participate in various competitions, and win exciting prizes. " +
+      //                       "Designed for creators, developers, and innovators, " +
+      //                       "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
+      //      .images(
+      //          new ArrayList<>() {{
+      //              add(
+      //                  ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
+      //                      .main(true)
+      //                      .name("somename.jpg").build()
+      //              );
+      //          }}
+      //      )
+      //      .creator(admin)
+      //      .build();
+//
+      //  ProjectEntity secondProject = ProjectEntity.builder()
+      //      .name("Volo")
+      //      .shortDescription("Volo is a web platform designed to help individuals and organizations raise funds for " +
+      //                        "charitable causes. It simplifies the donation process, allowing users to create campaigns," +
+      //                        " share them with others, and collect contributions securely")
+      //      .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
+      //                       "participate in various competitions, and win exciting prizes. " +
+      //                       "Designed for creators, developers, and innovators, " +
+      //                       "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
+      //      .images(
+      //          new ArrayList<>() {{
+      //              add(
+      //                  ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
+      //                      .name("somename.jpg")
+      //                      .main(true)
+      //                      .build()
+      //              );
+      //              add(
+      //                  ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
+      //                      .name("somename.jpg")
+      //                      .main(false)
+      //                      .build()
+      //              );
+//
+      //              add(
+      //                  ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
+      //                      .name("somename.jpg")
+      //                      .main(false)
+      //                      .build()
+      //              );
+//
+      //              add(
+      //                  ImageEntity.builder().url("https://voloapparel.com/cdn/shop/files/instagramlogo5_480x.jpg?v=1614245885")
+      //                      .name("somename.jpg")
+      //                      .main(false)
+      //                      .build()
+      //              );
+      //          }}
+      //      )
+      //      .creator(admin)
+      //      .build();
+//
+      //  admin.addProject(firstProject);
+      //  admin.addProject(secondProject);
+      //  for (int i = 0; i < 19; i++) {
+      //      admin.addProject(firstProject);
+      //      firstProject = ProjectEntity.builder()
+      //          .name("Competitors")
+      //          .shortDescription("Competitors is an innovative platform where users can showcase their projects, " +
+      //                            "participate in various competitions, and win exciting prizes. Designed for creators, developers, and innovators.")
+      //          .fullDescription("Competitors is an innovative platform where users can showcase their projects, " +
+      //                           "participate in various competitions, and win exciting prizes. " +
+      //                           "Designed for creators, developers, and innovators, " +
+      //                           "it allows users to create detailed profiles for their projects, including descriptions, images, and key features.")
+      //          .images(
+      //              new ArrayList<>() {{
+      //                  add(
+      //                      ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
+      //                          .main(true)
+      //                          .name("somename.jpg").build()
+      //                  );
+      //                  add(
+      //                      ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
+      //                          .main(false)
+      //                          .name("somename.jpg").build()
+      //                  );
+      //                  add(
+      //                      ImageEntity.builder().url("https://static.vecteezy.com/system/resources/previews/008/296/131/non_2x/trophy-icon-isolated-on-white-background-free-vector.jpg")
+      //                          .main(false)
+      //                          .name("somename.jpg").build()
+      //                  );
+      //              }}
+      //          )
+      //          .creator(admin)
+      //          .build();
+      //  }
+      //  if (userRepository != null) {
+      //      userRepository.save(admin);
+      //  }
+//
+      //  CompetitionEntity competitionEntity = CompetitionEntity.builder()
+      //      .name("first competition")
+      //      .description("first competition description")
+      //      .votingEndDate(Instant.MAX)
+      //      .organizer(admin)
+      //      .build();
+//
+      //  competitionRepository.saveAndFlush(competitionEntity);
     }
 }
